a6a3fb9cea4bac690e78079dd7330e5aa9bda05b
tenzinleshy <tenzinleshy@gmail.com>
2017-06-02 01:07:27 +0300

    add project files

---
+++ config/web.php
@@ -0,0 +1,153 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'qCavPNEDJelK9e0dqb5SXLoir-Zib7mU',
+            'baseUrl'=> '',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'budyaga\users\models\User',//'app\models\User',
+            'enableAutoLogin' => true,
+            'loginUrl' => ['/login'],//added
+        ],
+        'authClientCollection' => [//added
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'vkontakte' => [
+                    'class' => 'budyaga\users\components\oauth\VKontakte',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                    'scope' => 'email'
+                ],
+                'google' => [
+                    'class' => 'budyaga\users\components\oauth\Google',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'facebook' => [
+                    'class' => 'budyaga\users\components\oauth\Facebook',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'github' => [
+                    'class' => 'budyaga\users\components\oauth\GitHub',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                    'scope' => 'user:email, user'
+                ],
+                'linkedin' => [
+                    'class' => 'budyaga\users\components\oauth\LinkedIn',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'live' => [
+                    'class' => 'budyaga\users\components\oauth\Live',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'yandex' => [
+                    'class' => 'budyaga\users\components\oauth\Yandex',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'twitter' => [
+                    'class' => 'budyaga\users\components\oauth\Twitter',
+                    'consumerKey' => 'XXX',
+                    'consumerSecret' => 'XXX',
+                ],
+            ],
+        ],
+        'authManager' => [//added
+            'class' => 'yii\rbac\DbManager',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+//            'transport' => [
+//                'class' => 'Swift_SmtpTransport',
+//                'host' => 'localhost',
+//                'username' => 'account@domain.ru',
+//                'password' => 'password',
+//                'port' => '587',
+//                'encryption' => 'tls',
+//            ],
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [//added
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '/signup' => '/user/user/signup',
+                '/login' => '/user/user/login',
+                '/logout' => '/user/user/logout',
+                '/requestPasswordReset' => '/user/user/request-password-reset',
+                '/resetPassword' => '/user/user/reset-password',
+                '/profile' => '/user/user/profile',
+                '/retryConfirmEmail' => '/user/user/retry-confirm-email',
+                '/confirmEmail' => '/user/user/confirm-email',
+                '/unbind/<id:[\w\-]+>' => '/user/auth/unbind',
+                '/oauth/<authclient:[\w\-]+>' => '/user/auth/index'
+            ],
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'modules' => [//added
+        'user' => [
+            'class' => 'budyaga\users\Module',
+            'userPhotoUrl' => 'http://example.com/uploads/user/photo',
+            'userPhotoPath' => '@web/uploads/user/photo'
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
